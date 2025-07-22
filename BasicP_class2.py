ea84d444088f88afe81fadf603e62ee445f66e54
P <191jingjung@gmail.com>
2025-07-22 16:15:48 +0700

    add BasicP_class2.py and sunkwan.py, modiflied README.md

---
+++ BasicP_class2.py
@@ -0,0 +1,37 @@
+# x = 10
+# print("ค่าของx คือ; ", x)
+# y = 20
+# print("ค่าของy คือ; ", y)
+# print(type(x))
+# num = "1"
+# print("ค่าของnum คือ; ", num)
+# print(type(num))
+# num = int(num)
+# print("ค่าของnum หลังจากแปลงเป็น int คือ; ", num)
+# print(type(num))
+# ten = 10
+# one = 1k88880
+# print(ten  < one)
+# T = True
+# F = False
+# print(not T or F)
+# num = int(input("กรุณากรอกตัวเลข: "))
+# print("ค่าของnum คือ; ", num)
+# num1 = int(input("กรุณากรอกตัวเลข 1: "))
+# num2 = int(input("กรุณากรอกตัวเลข 2: "))
+# print(num1 > num2)
+
+# x = 10
+# y = 20 
+# if x == y:
+#     print("x เท่ากับ y")
+# elif x < y:
+#     print("x น้อยกว่า y")
+# elif x > y:
+#     print("x มากกว่า y")
+# else:
+#     print("x ไม่เท่ากับ y")
+
+# print("outside if: ", "staterpack yai mak")
+
+
